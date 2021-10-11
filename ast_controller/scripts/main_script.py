Fix marker z-axis rotation, marker poses and msgs type

ast_controller/scripts/main_script.py
@@ -11,6 +11,7 @@ from tf.transformations import quaternion_from_euler, quaternion_multiply
 import math as m
 import numpy as np
 import pickle as pkl
+from scipy.spatial.transform import Rotation as R
 
 
 class Pose:
@@ -40,23 +41,30 @@ class Controller:
    
     _cm_z = [0.5, 0.5, 0.5, 0.5]
 
-    _base_dist = 1.25
+    _base_dist = 0.5
 
     _cm_angles = [0.0, 12.5, 25.0, 37.5]
     _cm_angles_rad = np.radians(_cm_angles)
 
+    # _cm_x = [
+    #     -_base_dist*m.cos(_cm_angles_rad[0]),
+    #     -_base_dist*m.cos(_cm_angles_rad[1]),
+    #     -_base_dist*m.cos(_cm_angles_rad[2]),
+    #     -_base_dist*m.cos(_cm_angles_rad[3])
+    # ]
+
     _cm_x = [
-        -_base_dist*m.cos(_cm_angles_rad[0]),
-        -_base_dist*m.cos(_cm_angles_rad[1]),
-        -_base_dist*m.cos(_cm_angles_rad[2]),
-        -_base_dist*m.cos(_cm_angles_rad[3])
+        -_base_dist,
+        -_base_dist,
+        -_base_dist,
+        -_base_dist
     ]
 
     _cm_y = [ 
-        _base_dist*m.sin(_cm_angles_rad[0]),
-        _base_dist*m.sin(_cm_angles_rad[1]),
-        _base_dist*m.sin(_cm_angles_rad[2]),
-        _base_dist*m.sin(_cm_angles_rad[3])
+        _base_dist*m.tan(_cm_angles_rad[0]),
+        _base_dist*m.tan(_cm_angles_rad[1]),
+        _base_dist*m.tan(_cm_angles_rad[2]),
+        _base_dist*m.tan(_cm_angles_rad[3])
     ]
 
     _angle_boundary = 30
@@ -68,13 +76,9 @@ class Controller:
         output_lin = list()
         output_ang = list()
 
-        # if(self._flag == None):
-        #     return "dump"
-
         for i in range(0, 1):
-            xyz = data[i+3].tvecs_m
-            zc = data[i+3].zz_cos
-            zs = data[i+3].zz_sin
+            xyz = data[i].tvecs_m
+            z_vec = data[i].z_rot_m
             sign = 1
 
             distance_err = np.sqrt(
@@ -82,16 +86,15 @@ class Controller:
                               + pow(xyz[1]                , 2)
                               + pow(xyz[2] + self._cm_x[i], 2)
                                   )
-            
-            sign = np.sign(np.arctan2(zs, zc))
 
-            angle_err = -sign*180 + np.degrees(np.arctan2(zs, zc)) \
-                      - self._cm_angles[i] \
-                      + self._offset
+            rot = R.from_euler('y', 180 -self._cm_angles[i] + self._offset, degrees=True)
+
+            rotated_z = rot.apply(z_vec)
+
+            angle_err = np.degrees(np.arccos(rotated_z[2]))
             
             angle_err = abs(angle_err)
 
-            # angle_err = np.degrees(angle_err)
             output_lin.append(distance_err)
             output_ang.append(angle_err)
 
@@ -102,7 +105,6 @@ class Controller:
     def call_detector(self, msg):
 
         if(self._flag == None):
-            # print("*** Not ready yet ***")
             return "dump"
 
         data = dict(sorted(zip(msg.markerIds_m, msg.poses_m), key = lambda x: x[0]))
@@ -143,6 +145,7 @@ class Controller:
 
 
     def __init__(self, batch_in=10, deg_in=30, distance_to_cm=1.0):
+        self._flag = None
         rospack = rospkg.RosPack()
         self._package_path = rospack.get_path('ast_controller')    
         
@@ -195,17 +198,14 @@ class Controller:
             self.pose_pub(quat, "cuboid_marker5", cm5_pose)
             self.pose_pub(quat, "cuboid_marker6", cm6_pose)
 
+
+            time.sleep(0.017)
             self._flag = 1
             
             while(self._batch_counter < self._batch_size):
                 time.sleep(0.1)
 
             if(self._batch_counter == self._batch_size):
-                # for x in self._output_data:
-                #     self._file.write(str(x) + '\n')
-                
-                # self._output_data = list()
-                # self._batch_counter = 0
                 self._offset_list.append(self._offset)
                 self._linear_err_list.append(self._linear_err_list_current)
                 self._angular_err_list.append(self._angular_err_list_current)
@@ -216,7 +216,7 @@ class Controller:
 
             
 
-            self._offset = self._offset + 0.5
+            self._offset = self._offset + 1.0
             print("Another batch! #" + str(counter))
             counter += 1
             